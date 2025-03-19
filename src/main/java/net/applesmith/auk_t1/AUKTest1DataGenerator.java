ea8f0b8ba866418e650fae033550d71501c95aed
AppleSmith <applesmithspider@gmail.com>
2025-03-19 11:23:04 -0700

    Initial commit

---
+++ src/main/java/net/applesmith/auk_t1/AUKTest1DataGenerator.java
@@ -0,0 +1,11 @@
+package net.applesmith.auk_t1;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AUKTest1DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/applesmith/auk_t1/AUK_T1ModClient.java
@@ -0,0 +1,11 @@
+package net.applesmith.auk_t1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AUK_T1ModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
